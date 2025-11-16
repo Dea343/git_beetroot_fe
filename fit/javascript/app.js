2f62483c2acba313892ae70e1e30d732b021c9d4
Dea343 <dea34377@gmail.com>
2025-11-16 15:47:19 +0100

    detyra

---
+++ fit/javascript/app.js
@@ -0,0 +1,95 @@
+// var result = 2+2;
+
+// var resultText = `Rezultani nga 2+2=${result}`
+
+// console.log(resultText);
+
+// var userInput= prompt('Vlera 1 me mbledhje');
+// var userInput2= prompt('Vlera 2 1 me mbledhje');
+// var userResult = userInput*1 + userInput2 *1;
+
+
+// console.log('Vlera qe e ka shkruajtur user:' + userInput + userInput2)
+ 
+// let userName = prompt('Your Name');     
+// alter(`Your name is`)
+
+// const currentYear = 2025;
+// let yob = prompt('Enter your birthday');
+// let age = currentYear - yob;
+// alert(`Your age is:${age}`);
+
+// let a = prompt('Enter side A length');
+// let area = a*a;
+// console.log("Area is" + area);
+// alert(`Area is: `+area);
+
+// const PI = 3.14;
+// let radius = prompt('Enter radius');
+// let area = PI * (radius * radius);
+
+// console.log(`Area is: `+area);
+
+// let distance = prompt('Enter distance KM:');
+// let time = prompt('Enter the time to cover distane HR:');
+// let speed = distance / time;
+// console.log(`Speed is:` + speed +'KM/H');
+
+// const EX_RATE = 0.86;
+// let currencyUSD = prompt('Enter value in dollars:');
+// let currencyEur = currencyUSD + EX_RATE;
+// console.log('Value in EUR:' + currencyEur);
+
+// const SIZE_TO_COUNT = 820;
+// let valueInGB = prompt('Enter');
+// let valueInMb = valueInGB * 1024;
+// let numberOfFiles = valueInMb /SIZE_TO_COUNT + 'Number of files';
+// console.log(numberOfFiles);
+
+// let wallet, priceOfChocolate, nrOfBars, change;
+// priceOfChocolate = 2;
+
+// wallet = prompt('How much money do you have');
+// nrOfBars = wallet / priceOfChocolate;
+// change = wallet - (nrOfBars * priceOfChocolate);
+// console.log(`number of bars: ${nrOfBars} and change: ${change}`);
+
+// let a+b;
+// a= prompt('Enter first number') +1;
+// b= prompt('Enter second number') +1;
+
+// console.log(`AVG Mean: ${(a +b) /2}`);
+
+// let firstValue, secondValue;
+
+// firstValue = prompt ('enter first number')*1;
+// secondValue = prompt ('enter second number')*1;
+
+// console.log(`${firstValue} + ${secondValue} = ${firstValue + secondValue}`);
+// console.log(`${firstValue} - ${secondValue} = ${firstValue - secondValue}`);
+// console.log(`${firstValue} * ${secondValue} = ${firstValue * secondValue}`);
+// console.log(`${firstValue} / ${secondValue} = ${firstValue / secondValue}`);
+
+// const SALARY =250;
+// let sales = prompt('Total sales for current moth:')*1;
+// let bonus = sales *0.1;
+// let totalSalary = SALARY + bonus;
+
+// console.log(`Rroga e 13 ${totalSalary}`);
+
+//---------------------------------------------------
+
+let a = 100;
+let b = 12.345;
+let c = 100/0;
+let d= Infinity;
+let e = 'string'/100;
+let f
+
+= NaN ;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
